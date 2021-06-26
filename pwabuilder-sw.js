153ea6bae56c34f7c3bf0ba5f0e3da4994d036a7
Leonard Wong <me@leonardwong.net>
2021-06-26 16:06:47 +0800

    v21.24
    More enhancements to PWA

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -52,3 +52,13 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+async function registerPeriodicNewsCheck() {
+  const registration = await navigator.serviceWorker.ready;
+  try {
+    await registration.periodicSync.register('get-latest-news', {
+      minInterval: 24 * 60 * 60 * 1000,
+    });
+  } catch {
+    console.log('Periodic Sync could not be registered!');
+  }
+}
